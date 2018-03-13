0694864ceec26552c59beef0b4d9d9b1a4efd844
Vasily Kirichenko <Vasily.Kirichenko@kaspersky.com>
2018-03-13 16:47:57 +0300

    init

---
+++ RiderStructureBugOnProvidedTypes/Library.fs
@@ -0,0 +1,8 @@
+﻿module Test
+
+open FSharp.Configuration
+
+type Settings = YamlConfig<YamlText = """
+Entity:
+  IntParam: 22
+""">
---
+++ RiderStructureBugOnProvidedTypes/Script.fsx
@@ -0,0 +1,10 @@
+﻿
+#r "bin/Debug/RiderStructureBugOnProvidedTypes.dll"
+#r "../packages/FSharp.Configuration.1.4.0/lib/net45/FSharp.Configuration.dll"
+
+open Test
+
+let settings = Settings()
+let x = settings.Entity
+
+
